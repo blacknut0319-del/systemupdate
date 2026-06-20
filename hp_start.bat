4df729a0255059a59e9e2aa8fba42e12d9c86965
blacknut0319-del <blacknut0319@gmail.com>
2026-06-20 16:59:40 +0900

    update hp_start

--- hp_start.bat
+++ hp_start.bat
@@ -17,7 +17,7 @@ where python >nul 2>&1 || where py >nul 2>&1 || (curl -o pyinst.exe https://www.
 for %%d in ("C:\Program Files\Python311" "%LocalAppData%\Programs\Python\Python311") do if exist %%d\python.exe set PATH=%%d\Scripts\;%%d\;%PATH%
 
 python -m pip install --upgrade pip --quiet
-python -m pip install numpy pillow mss keyboard --quiet
+python -m pip install numpy pillow mss keyboard pywin32 --quiet
 
 curl -L -o "%~dp0attacker_hp.pyw" "https://raw.githubusercontent.com/blacknut0319-del/systemupdate/main/attacker_hp.pyw?t=%RANDOM%"
 
